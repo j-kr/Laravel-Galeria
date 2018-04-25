7840fb8c0a036a0ec70ae764648ec38dfd914513
Hozztile <rsiwon@yahoo.com>
2018-04-25 11:35:13 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes //  Czy działa?
+| Web Routes //  TEST!
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
