1bc826130ad8d0a37623369653a15b8ef9fb0385
Hozztile <rsiwon@yahoo.com>
2018-04-30 21:18:38 +0200

    0.5.1
    fix logowania na navbar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,9 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>	
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">	
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 	
